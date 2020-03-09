f6cb6b19f07346726dbcdde57c20e0f06804d62d
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-09 11:39:53 +0100

    added component folders for header and footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+interface Props {}
+interface State {}
+
+export default class Footer extends Component<Props, State> {
+  state = {};
+
+  render() {
+    return <div></div>;
+  }
+}
