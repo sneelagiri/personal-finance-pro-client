022a8ead9949c5467329d2df96f64f93c18bf5da
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-09 16:37:25 +0100

    added pie chart to homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -3,15 +3,47 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-
 import "./homepage.css";
+import CanvasJSReact from "../../assets/canvasjs.react";
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 interface Props {}
+
 interface State {}
 
 export default class Homepage extends Component<Props, State> {
   state = {};
 
   render() {
+    const options = {
+      exportEnabled: true,
+      animationEnabled: true,
+      title: {
+        text: "Budget - March 2020: €2,235.00"
+      },
+      data: [
+        {
+          type: "pie",
+          showInLegend: "true",
+          legendText: "{label}",
+          indexLabelFontSize: 16,
+          indexLabel: "{label} - #percent%",
+          percentFormatString: "#0.##",
+          toolTipContent: "{y} (#percent%)",
+          dataPoints: [
+            { y: 670.5, label: "Savings Target" },
+            { y: 340.84, label: "Transportation" },
+            { y: 657.09, label: "Rent" },
+            { y: 234.68, label: "Groceries" },
+            { y: 93.87, label: "Clothing" },
+            { y: 111.75, label: "Donations" },
+            { y: 46.94, label: "Misc." },
+            { y: 79.34, label: "Remaining Budget" }
+          ]
+        }
+      ]
+    };
     return (
       <Container fluid>
         <Row>
@@ -32,7 +64,9 @@ export default class Homepage extends Component<Props, State> {
               <li>Fast, simple, and responsive</li>
             </ul>
           </Col>
-          <Col></Col>
+          <Col>
+            <CanvasJSChart options={options} />
+          </Col>
         </Row>
         <Row>
           <Col>
