fb8536189d7a923de1127cffc65f7b29f0b823af
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-09 16:04:32 +0100

    included Header and Footer in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
