63002964bebf96abb12bc94fe09803ea6d72ab83
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-09 11:37:41 +0100

    added BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
