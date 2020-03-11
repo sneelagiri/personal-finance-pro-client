73fc88235897055b6ee9a1c2c1cac59c985bb1cf
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-11 10:17:57 +0100

    added signup and login as routes to App. Changed from functional comp to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 
-function App() {
-  return (
-    <div className="App">
+interface Props {}
+interface State {}
+
+export default class App extends Component<Props, State> {
+  state = {};
+
+  render() {
+    return (
       <Router>
         <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
         <Footer />
       </Router>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
